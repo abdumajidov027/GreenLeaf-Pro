1bd246aa5980012a9681f18204422c9a0fc1fdd2
Muhammadsodiq Abdumajidov <muhammadsodiqabdumajitov986@gmail.com>
2026-07-17 11:01:43 +0500

    Sprint 1 - data.js

---
+++ js/data.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 1,
+    name: "Krill Oil",
+    category: "Yurak",
+    price: 420000,
+    pv: 35,
+    image: "images/products/krill-oil.jpg",
+    description: "Omega-3 ga boy premium Krill Oil."
+  },
+  {
+    id: 2,
+    name: "Spirulina",
+    category: "Vitamin",
+    price: 280000,
+    pv: 20,
+    image: "images/products/spirulina.jpg",
+    description: "Tabiiy Spirulina, immunitetni qo'llab-quvvatlaydi."
+  },
+  {
+    id: 3,
+    name: "Cordyceps",
+    category: "Immunitet",
+    price: 350000,
+    pv: 30,
+    image: "images/products/cordyceps.jpg",
+    description: "Energiya va immunitet uchun."
+  },
+  {
+    id: 4,
+    name: "Ganoderma",
+    category: "Immunitet",
+    price: 390000,
+    pv: 28,
+    image: "images/products/ganoderma.jpg",
+    description: "Organizmni qo'llab-quvvatlovchi mahsulot."
+  },
+  {
+    id: 5,
+    name: "Calcium",
+    category: "Vitamin",
+    price: 260000,
+    pv: 18,
+    image: "images/products/calcium.jpg",
+    description: "Suyak va tishlar uchun kalsiy."
+  },
+  {
+    id: 6,
+    name: "Collagen",
+    category: "Go'zallik",
+    price: 450000,
+    pv: 40,
+    image: "images/products/collagen.jpg",
+    description: "Teri, soch va tirnoqlar uchun kollagen."
+  }
+];
